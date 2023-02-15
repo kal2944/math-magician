01898fe8dbac17bf6cc0422f79ad96a3d522bc1e
kdan2944 <kdan2944gmail.com>
2023-02-15 16:53:24 +0300

    update calculator.js file

--- src/component/Calculator.js
+++ src/component/Calculator.js
@@ -1,32 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-// eslint-disable-next-line react/prefer-stateless-function
-class Calculator extends Component {
-  render() {
-    return (
-      <div className="main">
-        <div className="screen">0</div>
-        <div className="btn light-gray">AC</div>
-        <div className="btn light-gray">+/-</div>
-        <div className="btn light-gray">%</div>
-        <div className="btn org">÷</div>
-        <div className="btn">7</div>
-        <div className="btn">8</div>
-        <div className="btn">9</div>
-        <div className="btn org">X</div>
-        <div className="btn">4</div>
-        <div className="btn">5</div>
-        <div className="btn">6</div>
-        <div className="btn org">-</div>
-        <div className="btn">1</div>
-        <div className="btn">2</div>
-        <div className="btn">3</div>
-        <div className="btn org">+</div>
-        <div className="btn zero">0</div>
-        <div className="btn">.</div>
-        <div className="btn org">=</div>
-      </div>
-    );
-  }
+function Calculator() {
+  return (
+    <div className="main">
+      <div className="screen">0</div>
+      <div className="btn light-gray">AC</div>
+      <div className="btn light-gray">+/-</div>
+      <div className="btn light-gray">%</div>
+      <div className="btn org">÷</div>
+      <div className="btn">7</div>
+      <div className="btn">8</div>
+      <div className="btn">9</div>
+      <div className="btn org">X</div>
+      <div className="btn">4</div>
+      <div className="btn">5</div>
+      <div className="btn">6</div>
+      <div className="btn org">-</div>
+      <div className="btn">1</div>
+      <div className="btn">2</div>
+      <div className="btn">3</div>
+      <div className="btn org">+</div>
+      <div className="btn zero">0</div>
+      <div className="btn">.</div>
+      <div className="btn org">=</div>
+    </div>
+  );
 }
+
 export default Calculator;
